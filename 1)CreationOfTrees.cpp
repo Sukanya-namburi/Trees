f07e15047341105e4135689559c64431b9dbb4c3
Sukanya-namburi <69515025+Sukanya-namburi@users.noreply.github.com>
2022-05-02 18:17:04 +0530

    Update 1)CreationOfTrees.cpp

--- 1)CreationOfTrees.cpp
+++ 1)CreationOfTrees.cpp
@@ -13,7 +13,7 @@ int main(){
     Node *root=new Node(1);                     
     root->left=new Node(2);                       
     root->right=new Node(3);                     
-    root->left->right=new Node(4);          
+    root->left->right=new Node(5);          
 }
   /*     (1)
          / \
