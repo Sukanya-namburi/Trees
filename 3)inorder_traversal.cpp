db2852c3038e926d6587e5608b523fac9f06dbc7
Sukanya-namburi <69515025+Sukanya-namburi@users.noreply.github.com>
2022-05-02 18:48:56 +0530

    Create 3)inorder_traversal.cpp

---
+++ 3)inorder_traversal.cpp
@@ -0,0 +1,36 @@
+//inorder traversal(left,root,right)
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+//logic for inorder traversal
+void inorder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);
+}
+int main(){
+    Node *root=new Node(1);                     
+    root->left=new Node(2);                       
+    root->right=new Node(3);                     
+    root->left->left=new Node(4); 
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    inorder(root);
+}
+  /*      (1)
+        /     \
+      (2)     (3)
+      / \     /
+    (4) (5)  (6)    */
+    
